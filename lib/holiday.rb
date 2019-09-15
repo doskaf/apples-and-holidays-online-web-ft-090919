e17842fda2e4e08543a097ea193dfa69337915cb
Dominique Skaf <doskaf@yahoo.com>
2019-09-15 20:50:31 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,8 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holiday|
     puts "#{season.capitalize}:"
-    holiday.each do |l
+    holiday.each do |holiday, supplies|
+      
 end
 
 def all_holidays_with_bbq(holiday_hash)
