1bbb8a0bc3dd22600847b91931b82d1d2368d0ed
Dominique Skaf <doskaf@yahoo.com>
2019-09-15 20:34:07 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -38,8 +38,8 @@ def add_supply_to_memorial_day(holiday_hash, supply)
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
-  holiday_hash.each do |season, holiday|
-     holiday << holiday_name => supply_array
+  holiday_hash.each do |season|
+    season[:holiday_name] = supply_array
    end
    holiday_hash
 end
