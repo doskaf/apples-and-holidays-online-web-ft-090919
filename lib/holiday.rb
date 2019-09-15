3916c5e70744ebc4191e2c18f9f8901539916a95
Dominique Skaf <doskaf@yahoo.com>
2019-09-15 20:43:10 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -44,7 +44,10 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-
+  winter_supplies = holiday_hash[:winter].map do |holidays, supplies|
+    supplies
+  end
+  winter_supplies.flatten
 end
 
 def all_supplies_in_holidays(holiday_hash)
@@ -55,10 +58,7 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
-  winter_supplies = holiday_hash[:winter].map do |holidays, supplies|
-    supplies
-  end
-  winter_supplies.flatten
+
 end
 
 def all_holidays_with_bbq(holiday_hash)
