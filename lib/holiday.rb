f2c986aa725b0b3d9bbda3cb5a2b9210b4b5029c
Dominique Skaf <doskaf@yahoo.com>
2019-09-15 20:37:24 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -57,7 +57,7 @@ def all_supplies_in_holidays(holiday_hash)
   # etc.
   holiday_hash.each do |season, holidays|
     if season == "Winter"
-      
+      holidays.values.flatten
 end
 
 def all_holidays_with_bbq(holiday_hash)
